a74ca64afdb4848aaefdb9b9256691d84d6e04ca
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2025-08-09 08:36:41 +0530

    initial lazyvim config with noice-dismiss using ESC

---
+++ lua/plugins/noice-dismiss.lua
@@ -0,0 +1,8 @@
+print("hello from noice-dismiss.lua")
+vim.keymap.set("n", "<Esc>", function()
+  if require("noice.lsp").scroll then
+    vim.cmd("NoiceDismiss")
+  else
+    vim.cmd("stopinsert")
+  end
+end, { silent = true })
