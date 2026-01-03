6e1c8975292b8041f88f20c0bf3ed09aac0163c0
Sanjay Achar <m.sanjayachar@gmail.com>
2026-01-03 13:22:13 +0530

    more comment annotations and telescope view for comment annotations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>h", function()
+  require("telescope.builtin").live_grep({ default_text = "ATHERE" })
+end)
+
+vim.keymap.set("n", "<leader>H", function()
+  require("telescope.builtin").live_grep({
+    default_text = "TODO:|FIX:|FIXME:|BUG:|HACK:|NOTE:|WARN:|WARNING:|PERF:|OPTIMIZE:|SECURITY:|DEPRECATED:|TEMP:|REVIEW:|QUESTION:",
+  })
+end)
---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/todo-comments.nvim",
+  opts = {
+    keywords = {
+      SECURITY = {
+        icon = "",
+        color = "#ff006e", -- sharp magenta-red (stands out, high risk)
+      },
+      DEPRECATED = {
+        icon = "",
+        color = "#ffbe0b", -- amber/yellow (sunset, end-of-life)
+      },
+      TEMP = {
+        icon = "",
+        color = "#3a86ff", -- cool blue (temporary, not urgent-danger)
+      },
+      REVIEW = {
+        icon = "",
+        color = "#8338ec", -- purple (human attention needed)
+      },
+      QUESTION = {
+        icon = "",
+        color = "#06d6a0", -- teal/green (curiosity, uncertainty)
+      },
+      ATHERE = {
+        icon = "",
+        color = "#ffd166", -- teal/green (curiosity, uncertainty)
+      },
+    },
+  },
+}
