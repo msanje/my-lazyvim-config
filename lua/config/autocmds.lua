7df881df17f33321dc2f8e75303839d1b7975787
Sanjay Achar <m.sanjayachar@gmail.com>
2026-01-09 08:59:49 +0530

    feat: add undotree toggle resize to default size

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,3 +14,10 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "undotree",
+  callback = function()
+    vim.cmd("vertical resize 35")
+  end,
+})
